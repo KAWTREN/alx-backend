cc5709ba39c9690730ba07bb79e2839abb90533c
kawtar <kawtarennabirha@gmail.com>
2024-07-05 11:36:15 +0100

    task0

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+"""doc doc"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +7,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """doc doc """
     return render_template('0-index.html')
 
 
