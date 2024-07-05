283909c7823c485381bf9ac36484138ca18177f2
kawtar <kawtarennabirha@gmail.com>
2024-07-05 17:14:39 +0100

    2

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -17,7 +17,7 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
-@babel.localselector
+@babel.localeselector
 def get_locale():
     """DOC DOC"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
