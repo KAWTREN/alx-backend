d709cfc939276472a8688c7fad75e07af2795417
kawtar <kawtarennabirha@gmail.com>
2024-07-05 17:09:03 +0100

    task2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""doc doc"""
+from flask import Flask, render_template, request
+from flask_babel import Babel  # type: ignore
+
+
+class Config:
+    """doc doc"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localselector
+def get_locale():
+    """DOC DOC"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route("/")
+def index():
+    """doc doc """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
